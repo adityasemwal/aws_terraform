131539a8fee2eaae715bfc01c41cc88150e09b3f
aditya semwal <aditya.semwal@nagarro.com>
2020-02-06 11:03:31 +0000

    pvt/pub keys moved to keys/ folder and instance.tf updated with keys

--- instance.tf
+++ instance.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "my_instance_1"{
   ami = var.AMIS[var.AWS_REGION]
   instance_type = var.AWS_INSTANCE
 }
+
+resource "aws_key_pair" "mykeypair"{
+  key_name = "adikey"
+  public_key = "${file("keys/adikey.pub")}"
+}
