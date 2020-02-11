46b831df64e192acee0f4bc62e0327041a7e0cbd
aditya semwal <aditya.semwal@nagarro.com>
2020-02-11 09:09:33 +0000

    route table added, security group added, instance details updated with same

--- instance.tf
+++ instance.tf
@@ -1,7 +1,14 @@
 resource "aws_instance" "my_instance_1"{
   ami = var.AMIS[var.AWS_REGION]
   instance_type = var.AWS_INSTANCE
-  key_name = aws_key_pair.mykeypair.key_name
-  
+
+  # Public Key SSH
+  key_name = aws_key_pair.mykeypair.key_name 
+
+  # VPC Subnet
+  subnet_id = aws_subnet.test-public-1.id
+
+  # Security Group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 }
 
--- vars.tf
+++ vars.tf
@@ -29,5 +29,5 @@ variable "CIDR_BLOCK"{
 }
 
 variable "SUB_1_CIDR"{
-  default = "10.0.1.0/16"
+  default = "10.0.1.0/24"
 }
