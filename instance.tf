bd5f1d32dc09766b0f3f64ada768af7c28e17172
aditya semwal <aditya.semwal@nagarro.com>
2020-02-13 09:25:28 +0000

    userdata reduced and remote-exec added to instance.tf

--- instance.tf
+++ instance.tf
@@ -11,8 +11,31 @@ resource "aws_instance" "my_instance_1"{
   # Security Group
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
+  # Instance Role
+  iam_instance_profile = "ec2-ecr"
+
   #User Data
   user_data = file("userData.sh")
+
+  # Provisoner
+  provisioner "remote-exec"{
+    connection{
+      type = "ssh"
+      host = self.public_ip
+      private_key = file("./keys/adikey")
+      user = "ec2-user"
+    }
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker -y",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo service docker enable",
+      "sudo $(aws ecr get-login --no-include-email --region us-west-1)",
+      "sudo ocker pull 809367851270.dkr.ecr.us-west-1.amazonaws.com/demo:latest",
+      "sudo docker run -itd -p 8080:8080 809367851270.dkr.ecr.us-west-1.amazonaws.com/demo:latest",
+    ]
+  }
 
   tags = {
     Name = "Terraform Instance"
